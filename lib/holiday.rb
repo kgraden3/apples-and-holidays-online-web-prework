ae59942f0ccc4937b3808f66c242bdadbe5a5a18
Kyle Graden <kylegraden3@gmail.com>
2019-03-12 20:38:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash.each do |season, data|
     if season == :winter
       data.each do |holiday, supply|
-        winter_supplies << supply.values
+        winter_supplies << holiday.values
       end
     end
   end
