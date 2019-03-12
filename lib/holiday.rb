fdfcab4689468d079a47e31f41dc655ceed3027f
Kyle Graden <kylegraden3@gmail.com>
2019-03-12 19:47:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,8 +75,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |season, data|
-    bbq_array = [];
+  bbq_array = [];
+  holiday_hash.each do |season, data|  
     data.each do |holiday, supply|
       if supply == "BBQ"
         bbq_array.pop(holiday)
